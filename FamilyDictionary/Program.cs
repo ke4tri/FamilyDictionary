80ddee64643684694adf8104882f18e3778b3706
Wayne Collier <ke4tri@gmail.com>
2019-03-13 08:02:03 -0500

    first commit

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,22 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+
+            foreach (var (person, details) in myFamily)
+            {
+                Console.WriteLine($"This person {person} has these details : ");
+                foreach (var detail in details)
+                {
+                    Console.WriteLine(detail.Key);
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadLine();
         }
     }
 }
